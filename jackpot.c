1ba38ae99b7e048c0ad52ff12b952c1332fc4a40
Christoph Thiele <christhulhu@gmx.de>
2024-11-06 05:37:09 +0100

    Update jackpot.c
    added a wallet version so u can bet on your win.
    added functions to keep the code more clean

--- jackpot.c
+++ jackpot.c
@@ -4,43 +4,87 @@
 #include <windows.h>
 #include <conio.h>
 
-int main() {
-     SetConsoleOutputCP(1252); SetConsoleCP(1252); system("cls");
+int wallet = 100;
+
+void Initialize()
+{
+    SetConsoleOutputCP(1252);
+    SetConsoleCP(1252);
+    system("cls");
+
+    srand(time(NULL));
+}
+
+void greeting() {
+
+    printf("Herzlich willkommen!\n");
+    printf("Drücke eine beliebige Taste, damit es losgeht!\n\n");
+    getch();
+}
+
+int CountWallet()
+{
+    int bet;
+
+    printf("Du hast %d Credits zur Verfügung.\n", wallet);
+    printf("Wie viel möchtest du setzen? ");
+    fflush(stdin);
+    scanf("%d", &bet);
+
+    wallet = wallet - bet;
+
+    return bet;
+}
+
+int main()
+{
+    Initialize();
+    greeting();
 
     char slotpool[5] = {'x', '+', '$', '?', '§'};
     int anzahl_Zeichen = sizeof(slotpool) / sizeof(slotpool[0]);
-    srand(time(NULL)); // Beim Ausführen des Skripts wird immer eine zufällige Zahl generiert
     char ausgabe[3];
-    char wiederholen = "n";
-
-    printf("Herzlich willkommen!\n");
-    printf("Drücke eine beliebige Taste, damit es losgeht!\n");
-    getch();
+    char wiederholen = 'n';
 
     do {
-        for(int i=0;i<3;i++) {
+        int bet = CountWallet();
+
+        for(int i=0;i<3;i++)
+        {
 
             int random_index = rand() %5; // Zufälligen Index berechnen (von 0 bis 5 - 1)
-            char *slot = slotpool[random_index];
+            char slot = slotpool[random_index];
             ausgabe[i] = slot;
 
             printf("| %c ", ausgabe[i]);
             fflush(stdout);
-            if(i==2) {
+            if(i==2)
+                {
                     printf("|");
                     break;
-            } else
-            sleep(3);
+                } else
+                    sleep(1);
         }
         printf("\n\n");
         if(ausgabe[0] == ausgabe[1] && ausgabe[0] == ausgabe[2]) {
-            printf("JACKPOT!");
+            printf("JACKPOT!\n");
+            printf("Dein neues Guthaben beträgt %d.", wallet);
+            wallet = walle + bet * 2;
             }
-            else if(ausgabe[0] == ausgabe[1] ^ ausgabe[1] == ausgabe[2]){
-                printf("Du bekommst deinen Gewinn zurück!");
+            else if
+                ((ausgabe[0] == ausgabe[1] && ausgabe[0] != ausgabe[2]) ||
+                (ausgabe[0] == ausgabe[2] && ausgabe[0] != ausgabe[1]) ||
+                (ausgabe[1] == ausgabe[2] && ausgabe[1] != ausgabe[0]))
+                {
+                    printf("Du bekommst deinen Gewinn zurück!\n");
+                    wallet = wallet + bet;
+                    printf("Dein neues Guthaben beträgt %d.", wallet);
                 }
                 else
-                    printf("Verloren!");
+                {
+                    printf("Verloren!\n");
+                    printf("Dein neues Guthaben beträgt %d.", wallet);
+                }
 
         printf("\n\n");
 
